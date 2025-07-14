c1d0f3c25b8924f9679ce133c9acf181a2060a65
Amazon-Clone <monalisha.k559@gmail.com>
2025-07-14 13:30:49 +0530

    file updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const modalImg=document.querySelector("#modal-img");
 const closeBtn=document.querySelector(".close");
 const clickableImages=document.querySelectorAll(".clickable-image");
 
-"click operation on all images"
+// click operation on all images
 clickableImages.forEach(img=>{
     img.addEventListener("click",()=>{
         modal.style.display="block";
@@ -11,12 +11,12 @@ clickableImages.forEach(img=>{
     });
 });
 
-"close the modal"
+// close the modal
 closeBtn.onclick=function(){
     modal.style.display="none";
 };
 
-"close on clicking outside the image"
+// close on clicking outside the image
 window.onclick=function(event){
     if(event.target===modal){
         modal.style.display="none";
